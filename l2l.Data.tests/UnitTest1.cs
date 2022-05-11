b28d870d8e5c241b4d25b521cd5bd11679fe75c6
rupperto <rupperto75@gmail.com>
2022-05-11 09:43:16 +0200

    #4: Delete  Course.Equals implamantation

---
+++ l2l.Data.tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace l2l.data.tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
--- l2l.Data/Model/Course.cs
+++ l2l.Data/Model/Course.cs
@@ -1,49 +1,14 @@
 namespace l2l.Data.Model
 {
-    public class Course : IEquatable<Course>
+    public class Course
     {
 
         public int ID { get; set; }
         public string Name { get; set; }
 
-        public override bool Equals(object obj)
-        {
-            return Equals(obj as Course); //obj tipusbol cours tipust csinalunk   
-
-        }
-
-        public bool Equals(Course course)
-        {
-            if (null == course)
-            {
-                return false;
-
-            }
-            if (ID != course.ID || Name != course.Name)
-            {
-                return false;
-
-            }
-
-            return true;
-
-        }
-
-        public override int GetHashCode()
-        {
-            //gethascode implementacio, ha van tobb propertynk akkor szep sorban hozzarakhatjuk a tobbit is, de esetelg int32-nél mar tulcsordulas okozhat.
-            unchecked   // tulcsordulas eseten vedelmet nyujt
-            {
-                var hash = 27;
-                hash = (hash * 13) + ID.GetHashCode();
-                hash = (hash * 13) + Name.GetHashCode();
-                return hash;
-
-            }
 
 
 
-        }
     }
 
 
